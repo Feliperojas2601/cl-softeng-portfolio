162e3f4271ad00cc67e4c7b63a4e3c3da67bd0ec
Felipe Rojas <jrojasce@unal.edu.co>
2025-04-09 17:35:09 -0500

    Add signal

--- Angular/bases/src/app/pages/counter/counterPage.component.ts
+++ Angular/bases/src/app/pages/counter/counterPage.component.ts
@@ -1,25 +1,35 @@
 // Pages -> Componentes que ocupan toda la pantalla
 
-import { Component } from "@angular/core";
+import { Component, signal, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
     templateUrl: './counterPage.component.html',  // Ruta del archivo HTML que se usará en el componente
-    styleUrls: ['./counterPage.component.css'] 
+    styleUrls: ['./counterPage.component.css'], 
+    /* Estrategia de detección de cambios Zoneless
+    changeDetection: ChangeDetectionStrategy.OnPush // Cambia la estrategia de detección de cambios, se actualiza el valor de la signal cuando se cambia el valor de la variable que esta usando la signal
+    */
 })
 export class CounterPageComponent {
-
     public counter: number = 10;
+    // Signal, paradigma reactivo, se actualiza el valor de la signal y se actualiza el valor de la variable que esta usando la signal
+    // SolidJS impulsó el uso de signals y Angular se ha adaptado a esta nueva forma de trabajar
+    // Antes usaba ZoneJS, librería pesada (performance, debug). Ahora angular apunta ser Zoneless 
+    public counterSignal = signal(10); // Writable signal de tipo number
 
     increaseBy(value: number) {
         this.counter += value;
+        this.counterSignal.update(current => current + value); // Actualiza el valor de la signal, el parametro es una funcion que recibe el valor actual de la signal y devuelve el nuevo valor
     }
 
     reset() {
-        this.counter = 10;
+        this.counter = 0;
+        this.counterSignal.set(0); // Setea el valor de la signal
     }
 
-    decreaseBy(value: number) {
-        this.counter -= value;
+    constructor() {
+        /*setInterval(() => {
+            this.counter += 1; // Esto no se ve reflejado con el changeDetection: ChangeDetectionStrategy.OnPush no estamos usando ZoneJS
+            this.counterSignal.update(current => current + 1); // Esto se ve reflejado con el changeDetection: ChangeDetectionStrategy.OnPush
+        }, 2000);*/
     }
-    
 }
