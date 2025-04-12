ac7682e82819ae3c7fba79b91e1f78429e257bbe
Felipe Rojas <jrojasce@unal.edu.co>
2025-04-11 22:51:14 -0500

    Update app configuration: add HashLocationStrategy provider comment for URL handling in routing

--- Angular/bases/src/app/app.config.ts
+++ Angular/bases/src/app/app.config.ts
@@ -6,5 +6,10 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   // ProvideZoneChangeDetection define la estrategia de detección de cambios
   // ProvideRouter define las rutas de la aplicación
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    // HashStrategy para desplegar y tener el # en la url 
+    // { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
 };
