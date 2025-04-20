e5f9c1dc58f5b89dab025a8fade5e61aba98d0c8
Felipe Rojas <jrojasce@unal.edu.co>
2025-04-20 16:48:37 -0500

    Enhance country list display: format population with a badge for better visibility and import DecimalPipe for number formatting in the country list component.

--- Angular/country-app/src/app/country/components/countryList/countryList.component.ts
+++ Angular/country-app/src/app/country/components/countryList/countryList.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
+import { DecimalPipe } from '@angular/common';
 @Component({
   selector: 'country-list',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './countryList.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
