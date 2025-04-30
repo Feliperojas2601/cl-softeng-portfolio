8addcbd4f7212899075feeb6cad370bf62ef7a12
Felipe Rojas <jrojasce@unal.edu.co>
2025-04-29 20:58:39 -0500

    Refactor ByRegionPageComponent to use linkedSignal for state management and enhance routing: initialize selected region from query parameters and update navigation to maintain region in URL.

--- Angular/country-app/src/app/country/pages/byRegion/byRegionPage.component.ts
+++ Angular/country-app/src/app/country/pages/byRegion/byRegionPage.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, linkedSignal } from '@angular/core';
 import { SearchInputComponent } from '../../../shared/components/searchInput/searchInput.component';
 import { CountryListComponent } from '../../components/countryList/countryList.component';  
 import { Region } from '../../interfaces/region.interface';
 import { CountryService } from '../../services/country.service';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { of } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-by-region-page',
   imports: [SearchInputComponent, CountryListComponent],
@@ -23,12 +24,17 @@ export class ByRegionPageComponent {
         'Antarctic',
     ];
 
-    selectedRegion = signal<Region | null>(null);
+    activatedRoute = inject(ActivatedRoute);
+    router = inject(Router);
+    regionParam = this.activatedRoute.snapshot.queryParamMap.get('region') ?? '';
+
+    selectedRegion = linkedSignal<Region | null>(() => this.regionParam as Region | null);
 
     countriesByRegion = rxResource({
         request: () => ({ region: this.selectedRegion() }),
         loader: ({ request }) => {
             if(!request.region) return of([]);
+            this.router.navigate(['/country/by-region'], { queryParams: { region: request.region } });
             return this.countryService.searchCountryByRegion(request.region);
         },
     });
