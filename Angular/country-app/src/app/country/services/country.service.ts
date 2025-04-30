f75288aaaccd1cc43911465d477d09c7dd7d2f00
Felipe Rojas <jrojasce@unal.edu.co>
2025-04-29 20:21:39 -0500

    Add capital cache

--- Angular/country-app/src/app/country/services/country.service.ts
+++ Angular/country-app/src/app/country/services/country.service.ts
@@ -13,18 +13,19 @@ export class CountryService {
     private readonly baseUrl = 'https://restcountries.com/v3.1';
 
     // Almacenamiento en prop, no tenemos que pensar en una seña, no está relacionado con un cambio en el DOM
-    private readonly termCacheByCapital = new Map<string, Observable<Country[]>>();
+    private readonly termCacheByCapital = new Map<string, Country[]>();
+    private readonly termCacheByCountry = new Map<string, Country[]>();
 
     searchByCapital(term: string) {
-        if(this.termCacheByCapital.has(term)) {
-            console.log('Retornando del cache la respuesta: ', term);
-            return this.termCacheByCapital.get(term)!;
-        }
         term = term.trim().toLowerCase();
+        if(this.termCacheByCapital.has(term)) {
+            console.log('Retornando del cache la respuesta by capital: ', term);
+            return of(this.termCacheByCapital.get(term)!);
+        }
         return this.http.get<RestCountry[]>(`${this.baseUrl}/capital/${term}`).pipe(
             map(restCountries => restCountryArrayToCountryArray(restCountries)),
             // Añadir el observable al cache
-            tap(countries => this.termCacheByCapital.set(term, of(countries))),
+            tap(countries => this.termCacheByCapital.set(term, countries)),
             // Atrapa los errores y devuelve un observable de error, podemos usar el of o el throwError
             // catchError(error => of(error))
             // Excepciones del lado del service
@@ -34,10 +35,18 @@ export class CountryService {
 
     searchByCountry(term: string) {
         term = term.trim().toLowerCase();
+        if(this.termCacheByCountry.has(term)) {
+            console.log('Retornando del cache la respuesta by country: ', term);
+            return of(this.termCacheByCountry.get(term)!).pipe(
+                delay(1500)
+            );
+        }
         return this.http.get<RestCountry[]>(`${this.baseUrl}/name/${term}`).pipe(
             map(restCountries => restCountryArrayToCountryArray(restCountries)),
+            tap(countries => this.termCacheByCountry.set(term, countries)),
             // Ralentizar la respuesta
             delay(1500),
+            // Añadir el observable al cache
             catchError(error => throwError(() => 'No results found or error in search'))
         );
     }
