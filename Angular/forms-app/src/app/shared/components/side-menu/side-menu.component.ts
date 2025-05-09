0e4411d9ef992582195668a92142fc716c7343f7
Felipe Rojas <jrojasce@unal.edu.co>
2025-05-09 17:59:56 -0500

    Enhance layout and side menu functionality: update index.html to use Bootstrap container, restructure app.component.html for responsive design, and expand side-menu.component.html and side-menu.component.ts to include multiple categorized menu items for navigation.

--- Angular/forms-app/src/app/shared/components/side-menu/side-menu.component.ts
+++ Angular/forms-app/src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { ReactiveRoutes } from '../../../reactive/reactive.routes';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+
+interface MenuItem {
+    title: string;
+    route: string;
+}
+
+const reactiveItems = ReactiveRoutes[0].children ?? [];
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+    reactiveMenu: MenuItem[] = reactiveItems.map(route => ({
+        title: route.title as string,
+        route: `reactive/${route.path}`
+    }));
+
+    authMenu: MenuItem[] = [
+        {
+            title: 'Sign up',
+            route: './auth'
+        },
+    ]
+
+    countryMenu: MenuItem[] = [
+        {
+            title: 'Country',
+            route: './country'
+        },
+    ]
+}
