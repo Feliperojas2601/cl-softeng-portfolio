b957826e7b260b7184d141e9ca96449e8b662886
Felipe Rojas <jrojasce@unal.edu.co>
2025-05-09 17:45:10 -0500

    Update Angular project configuration and enhance app layout: add Bootstrap CSS for styling, include SideMenu component in app structure, and define new routing paths for reactive forms, countries, and authentication features.

--- Angular/forms-app/src/app/app.routes.ts
+++ Angular/forms-app/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'reactive',
+        loadChildren: () => import('./reactive/reactive.routes').then(m => m.ReactiveRoutes)
+    },
+    {
+        path: 'countries',
+        loadChildren: () => import('./country/country.routes').then(m => m.CountryRoutes)
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes').then(m => m.AuthRoutes)
+    },
+    {
+        path: '**',
+        redirectTo: 'reactive'
+    }
+]
