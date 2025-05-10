3ac5d961bc64fbe2650878529a93c1f5ddfa13f6
Felipe Rojas <jrojasce@unal.edu.co>
2025-05-10 16:50:16 -0500

    Refactor SwitchesPageComponent: implement reactive form with validation, update HTML for form controls and error handling, and translate labels to English.

--- Angular/forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ Angular/forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,28 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SwitchesPageComponent { }
+export class SwitchesPageComponent {
+    private readonly formBuilder: FormBuilder = inject(FormBuilder);
+    formUtils = FormUtils;
+    myForm: FormGroup = this.formBuilder.group({
+        gender: ['M', [Validators.required]],
+        notifications: [true, [Validators.required]],
+        termsAndConditions: [false, [Validators.requiredTrue]],
+    });
+
+    save() {
+        if (this.myForm.invalid) {
+            this.myForm.markAllAsTouched();
+            return;
+        }
+        console.log(this.myForm.value);
+        this.myForm.reset();
+    }
+}
