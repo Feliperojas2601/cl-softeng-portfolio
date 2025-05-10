5ff1f6c7aaa526aca1c5736aa4015df8f962f0e2
Felipe Rojas <jrojasce@unal.edu.co>
2025-05-10 16:30:07 -0500

    Enhance DynamicPageComponent: add form submission handling, implement add and delete game functionality, and improve error display for favorite games in the template.

--- Angular/forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ Angular/forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '../../../utils/form-utils';
 @Component({
   selector: 'app-dynamic-page',
@@ -20,8 +20,31 @@ export class DynamicPageComponent {
         ], [Validators.minLength(3)]),
     });
 
+    //Control aislado 
+    newFavoriteGame: FormControl = this.formBuilder.control('', [Validators.required, Validators.minLength(3)]);
+
     get favoriteGames() {
         // get función para obtener un control
         return this.myForm.get('favoriteGames') as FormArray;
+    }
+
+    addGame() {
+        if (this.newFavoriteGame.invalid) return;
+        const newGame = this.newFavoriteGame.value;
+        this.favoriteGames.push(this.formBuilder.control(newGame, [Validators.required, Validators.minLength(3)]));
+        this.newFavoriteGame.reset();
+    }
+
+    deleteGame(index: number) {
+        this.favoriteGames.removeAt(index);
+    }
+
+    save() {
+        if (this.myForm.invalid) {
+            this.myForm.markAllAsTouched();
+            return;
+        }
+        console.log(this.myForm.value);
+        this.myForm.reset();
     }
 }
