cf9dfa63c116e98ae81df0a5eb2e5b7f12a43322
Felipe Rojas <jrojasce@unal.edu.co>
2025-05-10 14:52:46 -0500

    Refactor BasicPageComponent to use FormBuilder for reactive forms: update form structure and validation, enhance HTML to display error messages and field values.

--- Angular/forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ Angular/forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -10,14 +10,21 @@ import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angula
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BasicPageComponent {
-    // Manera tradicional reactiva
+    // Manera tradicional reactiva group y control
     // Formgroup define la estructura del formulario
     // FormControl define el estado de cada campo del formulario y sus validaciones
-    myForm = new FormGroup({
+    /*myForm = new FormGroup({
         name: new FormControl<string>('', [Validators.required, Validators.minLength(3)]),
         price: new FormControl<number>(0, [Validators.required, Validators.min(0)]),
         inStock: new FormControl<number>(0, [Validators.required, Validators.min(0)]),
-    })
+    })*/
 
-    // Métodos para acceder a los campos del formulario
+    // Manera reactiva con FormBuilder, definición como objeto de javascript, para anidados es mejor
+    private readonly formBuilder: FormBuilder = inject(FormBuilder);
+    myForm = this.formBuilder.group({
+        // primer valor es el valor por defecto, segundo es el array de validaciones syncronas, tercer valor es el array de validaciones asincronas
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        price: [0, [Validators.required, Validators.min(1)]],
+        inStock: [0, [Validators.required, Validators.min(1)]],
+    })
 }
