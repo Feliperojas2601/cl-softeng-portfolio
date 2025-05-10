ab2c78b9dac99a1991c724d73a2152a13844aa7c
Felipe Rojas <jrojasce@unal.edu.co>
2025-05-10 14:43:01 -0500

    Implement reactive form in BasicPageComponent: update HTML to bind form controls and add validation, enhance TypeScript to define form structure and controls with validation rules.

--- Angular/forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ Angular/forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,23 @@
 import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  // ReactiveFormsModule es necesario para usar el formulario reactivo
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+    // Manera tradicional reactiva
+    // Formgroup define la estructura del formulario
+    // FormControl define el estado de cada campo del formulario y sus validaciones
+    myForm = new FormGroup({
+        name: new FormControl<string>('', [Validators.required, Validators.minLength(3)]),
+        price: new FormControl<number>(0, [Validators.required, Validators.min(0)]),
+        inStock: new FormControl<number>(0, [Validators.required, Validators.min(0)]),
+    })
+
+    // Métodos para acceder a los campos del formulario
+}
