3435d8e360a8a86474681a41536fcf9ec9d3f897
Felipe Rojas <jrojasce@unal.edu.co>
2025-06-08 12:20:42 -0500

    Implement reactive form in CountryPageComponent: update form structure with validation, enhance HTML for dynamic country selection based on region, and display form validity and value in the template.

--- Angular/forms-app/src/app/country/pages/country-page/country-page.component.ts
+++ Angular/forms-app/src/app/country/pages/country-page/country-page.component.ts
@@ -1,9 +1,54 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, effect, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CountryPageComponent { }
+export class CountryPageComponent {
+    fb = inject(FormBuilder);
+    myForm = this.fb.group({
+        region: ['', Validators.required],
+        country: ['', Validators.required],
+        border: ['', Validators.required],
+    });
+    countryService = inject(CountryService);
+    regions = signal<string[]>(this.countryService.regions);
+    countries = signal<Country[]>([]);
+    borders = signal<string[]>([]);
+
+    // Cuando cambie la region, se actualiza el formulario, se monta apenas se renderiza el componente
+    onFormChange = effect((onCleanup) => {
+        const regionSubscription = this.onRegionChanged();
+        onCleanup(() => {
+            regionSubscription.unsubscribe();
+        });
+    });
+
+    // Retorno una subscription para poder desuscribirme cuando se desmonte el componente
+    onRegionChanged() {
+        return this.myForm
+            .get('region')!
+            .valueChanges
+            .pipe(
+                tap((region) => {
+                    // Resetear el formulario
+                    this.myForm.get('country')!.setValue('');
+                    this.countries.set([]);
+                    this.myForm.get('border')!.setValue('');
+                    this.borders.set([]);
+                }), 
+                // transformo el valor de la region en un observable de paises
+                switchMap((region) => this.countryService.getCountriesByRegion(region!)),
+            )
+            .subscribe(countries => {
+                console.log('countries', countries);
+                this.countries.set(countries);
+            });
+    }
+}
